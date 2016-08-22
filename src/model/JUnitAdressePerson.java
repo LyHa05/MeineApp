cfb711dc1f9faca176fc0a73c066ddf71fcd9736
lpflug <lpflug@NTCG57.ntcg.local>
2016-08-22 10:18:27 +0200

    Klassen für Testfälle hinzugefügt

---
+++ src/model/JUnitAdressePerson.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lpflug
+ *
+ */
+public class JUnitAdressePerson {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
