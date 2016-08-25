29d6acba7a38de69a954fb971cccb895a1de468b
lpflug <lpflug@NTCG57.ntcg.local>
2016-08-25 16:38:39 +0200

    Update Statement: Fehler behoben...muhahaha

--- src/application/model/PersonDB.java
+++ src/application/model/PersonDB.java
@@ -39,31 +39,9 @@ public class PersonDB {
 
 	public static void aenderePerson(Connection verbindung, Person p) throws SQLException {
     	
-		ps = verbindung.prepareStatement("DECLARE @PersonID INT "
-				+ "Set @PersonID = ?"
-				+ "UPDATE Person SET "
-    			+ "Name = ?,"
-    			+ "Vorname1 = ?,"
-    			+ "Vorname2 = ?,"
-    			+ "Geschlecht = ?,"
-    			+ "Geburtsdatum = ?,"
-    			+ "HandyNr1 = ?,"
-    			+ "EMailAdresse1 = ?"
-    			+ "WHERE PersonID = @PersonID");
-      
-			 ps.setInt(1, p.getPersonID());
-			 ps.setString(2, p.getName());
-        	 ps.setString(3, p.getVorname1());
-        	 ps.setString(4, p.getVorname2());
-        	 ps.setString(5, p.getGeschlecht());
-        	 ps.setDate(6, java.sql.Date.valueOf(p.getGeburtsdatum()));
-        	 ps.setString(7, p.getHandyNr1());
-        	 ps.setString(8, p.geteMailAdresse1());
-        	 ps.executeUpdate();
-        	 
-        	 System.out.println("Update");
-		
-//    	try {ps = verbindung.prepareStatement("UPDATE Person SET "
+//		ps = verbindung.prepareStatement("DECLARE @PersonID INT "
+//				+ "Set @PersonID = ?"
+//				+ "UPDATE Person SET "
 //    			+ "Name = ?,"
 //    			+ "Vorname1 = ?,"
 //    			+ "Vorname2 = ?,"
@@ -71,29 +49,52 @@ public class PersonDB {
 //    			+ "Geburtsdatum = ?,"
 //    			+ "HandyNr1 = ?,"
 //    			+ "EMailAdresse1 = ?"
-//    			+ "WHERE PersonID = ?");
+//    			+ "WHERE PersonID = @PersonID");
 //      
-//        	 ps.setString(1, p.getName());
-//        	 ps.setString(2, p.getVorname1());
-//        	 ps.setString(3, p.getVorname2());
-//        	 ps.setString(4, p.getGeschlecht());
-//        	 ps.setDate(5, java.sql.Date.valueOf(p.getGeburtsdatum()));
-//        	 ps.setString(6, p.getHandyNr1());
-//        	 ps.setString(7, p.geteMailAdresse1());
-//        	 ps.setInt(8, p.getPersonID());
+//			 ps.setInt(1, p.getPersonID());
+//			 ps.setString(2, p.getName());
+//        	 ps.setString(3, p.getVorname1());
+//        	 ps.setString(4, p.getVorname2());
+//        	 ps.setString(5, p.getGeschlecht());
+//        	 ps.setDate(6, java.sql.Date.valueOf(p.getGeburtsdatum()));
+//        	 ps.setString(7, p.getHandyNr1());
+//        	 ps.setString(8, p.geteMailAdresse1());
 //        	 ps.executeUpdate();
 //        	 
 //        	 System.out.println("Update");
-//        	 
-//        } catch (SQLException ex) {
-////        	TODO Fehlerbehandlung ordentlich einrichten
-////            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null,
-////                    ex);
-//        	System.out.println("Kein Update");
-//        } finally {
-//            ps.close();
-////            verbindung.close();
-//        }		
+		
+    	try {ps = verbindung.prepareStatement("UPDATE Person SET "
+    			+ "Name = ?,"
+    			+ "Vorname1 = ?,"
+    			+ "Vorname2 = ?,"
+    			+ "Geschlecht = ?,"
+    			+ "Geburtsdatum = ?,"
+    			+ "HandyNr1 = ?,"
+    			+ "EMailAdresse1 = ? "
+    			+ "WHERE PersonID = ?");
+      
+        	 ps.setString(1, p.getName());
+        	 ps.setString(2, p.getVorname1());
+        	 ps.setString(3, p.getVorname2());
+        	 ps.setString(4, p.getGeschlecht());
+        	 ps.setDate(5, java.sql.Date.valueOf(p.getGeburtsdatum()));
+        	 ps.setString(6, p.getHandyNr1());
+        	 ps.setString(7, p.geteMailAdresse1());
+        	 ps.setInt(8, p.getPersonID());
+        	 ps.executeUpdate();
+        	 
+        	 System.out.println("Update");
+        	 
+        } catch (SQLException ex) {
+//        	TODO Fehlerbehandlung ordentlich einrichten
+//            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null,
+//                    ex);
+        	System.out.println(ex.getMessage());
+        	System.out.println("Kein Update");
+        } finally {
+            ps.close();
+//            verbindung.close();
+        }		
 	}
 	
 }
