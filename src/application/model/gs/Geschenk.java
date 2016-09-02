810126466067ecf0ba83d0970ab38eb33fb6ab47
Lydia <Lydia@Lydia>
2016-09-02 20:55:39 +0200

    Klasse Geschenk und GeschenkBestandteil

--- src/application/model/gs/Geschenk.java
+++ src/application/model/gs/Geschenk.java
@@ -4,6 +4,8 @@ import application.model.person.Person;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
@@ -27,6 +29,81 @@ public class Geschenk {
 	public Geschenk(int gID, int jahr, String anlass, String memo, int preis, Person person) {
 		this.geschenkID = new SimpleIntegerProperty(gID);
 		this.jahr = new SimpleIntegerProperty(jahr);
+		this.anlass = new SimpleStringProperty(anlass);
+		this.memo = new SimpleStringProperty(memo);
+		this.preis = new SimpleIntegerProperty(preis);
+		this.erhaelt = new SimpleObjectProperty<Person>(person);
 	}
 	
+	public int getGeschenkID() {
+		return geschenkID.get();
+	}
+
+	public void setGeschenkID(int geschenkID) {
+		this.geschenkID.set(geschenkID);
+	}
+
+    public IntegerProperty geschenkIDProperty() {
+        return geschenkID;
+    }
+    
+	public int getJahr() {
+		return jahr.get();
+	}
+
+	public void setJahrID(int jahr) {
+		this.jahr.set(jahr);
+	}
+
+    public IntegerProperty jahrProperty() {
+        return jahr;
+    }
+	
+	public String getAnlass() {
+		return anlass.get();
+	}
+
+	public void setAnlass(String anlass) {
+		this.anlass.set(anlass);
+	}
+
+    public StringProperty anlassProperty() {
+        return anlass;
+    }
+    
+	public String getMemo() {
+		return memo.get();
+	}
+
+	public void setMemo(String memo) {
+		this.memo.set(memo);
+	}
+
+    public StringProperty memoProperty() {
+        return memo;
+    }
+        
+    public int getPreis() {
+		return preis.get();
+	}
+
+	public void setPreis(int preis) {
+		this.preis.set(preis);
+	}
+
+    public IntegerProperty preisProperty() {
+        return preis;
+    }
+    
+    public Person getErhaelt() {
+    	return erhaelt.get();
+    }
+
+    public void setErhaelt(Person person) {
+    	this.erhaelt.set(person);
+    }
+    
+    public ObjectProperty<Person> personProperty() {
+    	return erhaelt;
+    }
 }
--- src/application/model/gs/GeschenkBestandteil.java
+++ src/application/model/gs/GeschenkBestandteil.java
@@ -1,5 +1,104 @@
 package application.model.gs;
 
+import application.model.person.Person;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class GeschenkBestandteil {
 
+	private IntegerProperty geschenkBestandteilID;
+	private StringProperty beschreibung;
+	private StringProperty memo;
+	private StringProperty kategorie;
+	private StringProperty bestandteil;
+	private ObjectProperty<Geschenk> bestandteilVon;
+	
+	public GeschenkBestandteil(int geschenkBestandteilID, String beschreibung, String memo,
+			String kategorie, String bestandteil, Geschenk bestandteilVon) {
+		
+		this.geschenkBestandteilID = new SimpleIntegerProperty(geschenkBestandteilID);
+		this.beschreibung = new SimpleStringProperty(beschreibung);
+		this.memo = new SimpleStringProperty(memo);
+		this.kategorie = new SimpleStringProperty(kategorie);
+		this.bestandteil = new SimpleStringProperty(bestandteil);
+		this.bestandteilVon = new SimpleObjectProperty<Geschenk>(bestandteilVon);
+	}
+	
+	public int getGeschenkBestandteilID() {
+		return geschenkBestandteilID.get();
+	}
+
+	public void setGeschenkBestandteilID(int geschenkBestandteilID) {
+		this.geschenkBestandteilID.set(geschenkBestandteilID);
+	}
+
+    public IntegerProperty geschenkBestandteilIDProperty() {
+        return geschenkBestandteilID;
+    }
+	
+    public String getBeschreibung() {
+		return beschreibung.get();
+	}
+
+	public void setBeschreibung(String beschreibung) {
+		this.beschreibung.set(beschreibung);
+	}
+
+    public StringProperty beschreibungProperty() {
+        return beschreibung;
+    }
+
+    public String getMemo() {
+		return memo.get();
+	}
+
+	public void setMemo(String memo) {
+		this.memo.set(memo);
+	}
+
+    public StringProperty memoProperty() {
+        return memo;
+    }
+    
+    public String getKategorie() {
+		return kategorie.get();
+	}
+
+	public void setKategorie(String kategorie) {
+		this.kategorie.set(kategorie);
+	}
+
+    public StringProperty kategorieProperty() {
+        return kategorie;
+    }
+
+    public String getBestandteil() {
+		return bestandteil.get();
+	}
+
+	public void setBestandteil(String bestandteil) {
+		this.bestandteil.set(bestandteil);
+	}
+
+    public StringProperty bestandteilProperty() {
+        return bestandteil;
+    }
+
+    public Geschenk getBestandteilVon() {
+    	return bestandteilVon.get();
+    }
+
+    public void setBestandteilVon(Geschenk geschenk) {
+    	this.bestandteilVon.set(geschenk);
+    }
+    
+    public ObjectProperty<Geschenk> bestandteilVonProperty() {
+    	return bestandteilVon;
+    }
+
+	
 }
