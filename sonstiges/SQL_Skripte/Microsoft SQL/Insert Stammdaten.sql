a2786206d65a8c77d4fd3f421a04a35bc9161567
lpflug <lpflug@NTCG57.ntcg.local>
2016-09-15 09:53:35 +0200

    Korrektur SQL INSERT Befehle für Stammdaten

--- sonstiges/SQL_Skripte/Microsoft SQL/Insert Stammdaten.sql
+++ sonstiges/SQL_Skripte/Microsoft SQL/Insert Stammdaten.sql
@@ -1,20 +1,22 @@
+USE test6
+
 INSERT INTO StammdatenKategorie (Kategorie)
-	VALUES("Geschlecht"), ("Land"), ("Anlass"), ("GeschenkKategorie"), ("Geschenkbestandteil");
+	VALUES('Geschlecht'), ('Land'), ('Anlass'), ('GeschenkKategorie'), ('Geschenkbestandteil');
 	
 INSERT INTO StammdatenWert (KategorieID, Wert)
-	VALUES(1,"Männlich"), (1,"Weiblich");
+	VALUES(1,'Männlich'), (1,'Weiblich');
 	
 INSERT INTO StammdatenWert (KategorieID, Wert)
-	VALUES(2,"Deutschland"), (2,"Österreich"), (2,"Schweiz");
+	VALUES(2,'Deutschland'), (2,'Österreich'), (2,'Schweiz');
 	
 INSERT INTO StammdatenWert (KategorieID, Wert)
-	VALUES(3,"Hochzeit"), (3, "Geburtstag"), (3, "Dankeschön"), (3, "Weihnachten"), (3, "Ostern");
+	VALUES(3,'Hochzeit'), (3, 'Geburtstag'), (3, 'Dankeschön'), (3, 'Weihnachten'), (3, 'Ostern');
 
 INSERT INTO StammdatenWert (KategorieID, Wert)
-	VALUES(4,"Gebastelt"), (4,"Gekauft");
+	VALUES(4,'Gebastelt'), (4,'Gekauft');
 	
 INSERT INTO StammdatenWert (KategorieID, Wert)
-	VALUES(5,"Karte"), (5,"Präsent");
+	VALUES(5,'Karte'), (5,'Präsent');
 	
 
 
