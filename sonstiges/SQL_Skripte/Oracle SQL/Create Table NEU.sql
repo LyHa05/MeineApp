a611f7ec8f8465a7e084bd247cd47527ab2b616e
Lydia <Lydia@Lydia>
2016-09-23 15:29:15 +0200

    SQL ORacle Table Insert korrigiert

---
+++ sonstiges/SQL_Skripte/Oracle SQL/Create Table NEU.sql
@@ -0,0 +1,56 @@
+Table WOHNHAFTIN gelöscht.
+
+
+Table ADRESSE gelöscht.
+
+
+Table GESCHENKBESTANDTEIL gelöscht.
+
+
+Table GESCHENK gelöscht.
+
+Table EMAIL gelöscht.
+
+
+Table PERSON gelöscht.
+
+
+Table STAMMDATENWERT gelöscht.
+
+Table STAMMDATENKATEGORIE gelöscht.
+
+
+View PERSONUEBERSICHT gelöscht.
+
+
+View GESCHENKUEBERSICHT gelöscht.
+
+
+Table STAMMDATENKATEGORIE erstellt.
+
+
+Table STAMMDATENWERT erstellt.
+
+
+Table PERSON erstellt.
+
+Table EMAIL erstellt.
+
+
+Table ADRESSE erstellt.
+
+
+Table WOHNHAFTIN erstellt.
+
+
+Table GESCHENK erstellt.
+
+
+Table GESCHENKBESTANDTEIL erstellt.
+
+
+View PERSONUEBERSICHT erstellt.
+
+
+View GESCHENKUEBERSICHT erstellt.
+
