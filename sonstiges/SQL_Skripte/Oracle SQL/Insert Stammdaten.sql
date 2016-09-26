7e51605c5e5237c0520a2776319b8cd099f47a7b
lydia <lydia@DESKTOP-1CSCC21.fritz.box>
2016-09-26 21:25:09 +0200

    Insert Oracle Stammdaten

---
+++ sonstiges/SQL_Skripte/Oracle SQL/Insert Stammdaten.sql
@@ -0,0 +1,29 @@
+INSERT INTO StammdatenKategorie (Kategorie) VALUES('Geschlecht');
+INSERT INTO StammdatenKategorie (Kategorie) VALUES('Land');
+INSERT INTO StammdatenKategorie (Kategorie) VALUES('Anlass');
+INSERT INTO StammdatenKategorie (Kategorie) VALUES('GeschenkKategorie');
+INSERT INTO StammdatenKategorie (Kategorie) VALUES('Geschenkbestandteil');
+
+
+
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(1,'Männlich');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(1,'Weiblich');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(2,'Deutschland');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(2,'Österreich');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(2,'Schweiz');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(3,'Hochzeit');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(3,'Geburtstag');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(3,'Dankeschön');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(3,'Weihnachten');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(3,'Ostern');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(4,'Gekauft');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(4,'Gebastelt');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(5,'Präsent');
+INSERT INTO StammdatenWert (KategorieID, Wert) VALUES(5,'Karte');
+
+
+	
+
+
+
+	
